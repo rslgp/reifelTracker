5285e70abd32219c354376441ee9398becad506d
rslgp <rslgp@cin.ufpe.br>
2017-12-12 22:07:54 -0300

    update valor perdoar contas novatos/antigas

--- bot.js
+++ bot.js
@@ -331,7 +331,7 @@ function up(text){
 	}
 	try{
 		//cap new accounts
-		if(jsonSquad[winP].value < 23){ //perdoar novatos q nao sao smurf
+		if(jsonSquad[winP].value < 26){ //perdoar novatos q nao sao smurf
 			return jsonSquad[winP].value;
 		}else{
 			if(jsonSquad[wins].ValueInt > 109){
