e7adc98ab0fc74a749ab8916c87cce5b7a085f01
rslgp <rslgp@cin.ufpe.br>
2018-01-17 16:06:05 -0300

    era um colchetes o bug

--- bot.js
+++ bot.js
@@ -372,27 +372,27 @@ client.on('message', message => {
 						print(message,"Parabéns! Você agora é <@&393260318434000907> \:trophy: \:ok_hand:");
 						
 					}else if(winrKD[0]>=30 && winrKD[1]>=4.3){//godlike
-						if(message.member.roles.has(godlike)) print(message,"continua onde está"); return;
+						if(message.member.roles.has(godlike)) {print(message,"continua onde está"); return;}
 						changeRole(message.member, legendary, godlike);	
 						print(message,"Parabéns! Você agora é <@&376840180688224257> \:trophy: \:ok_hand:");	
 						
 					}else if(winrKD[0]>=25 && winrKD[1]>=3.5){//lendario
-						if(message.member.roles.has(legendary)) print(message,"continua onde está"); return;
+						if(message.member.roles.has(legendary)) {print(message,"continua onde está"); return;}
 						changeRole(message.member, epic, legendary);
 						print(message,"Parabéns! Você agora é <@&373639920591306753> \:trophy: \:ok_hand:");	
 						
 					}else if(winrKD[0]>=20 && winrKD[1]>=2.7){//epic
-						if(message.member.roles.has(epic)) print(message,"continua onde está"); return;
+						if(message.member.roles.has(epic)){print(message,"continua onde está"); return;}
 						changeRole(message.member, rare, epic);
 						print(message,"Parabéns! Você agora é <@&373640006314754057> \:trophy: \:ok_hand:");
 						
 					}else if(winrKD[0]>=15 && winrKD[1]>=1.9){//rare
-						if(message.member.roles.has(rare)) print(message,"continua onde está"); return;
+						if(message.member.roles.has(rare)) {print(message,"continua onde está"); return;}
 						changeRole(message.member, incomum, rare);	
 						print(message,"Parabéns! Você agora é <@&373640089986924554> \:trophy: \:ok_hand:");
 						
 					}else if(winrKD[0]>=10 && winrKD[1]>=1.1){//incomum
-						if(message.member.roles.has(incomum)) print(message,"continua onde está"); return;
+						if(message.member.roles.has(incomum)) {print(message,"continua onde está"); return;}
 						changeRole(message.member, desconhecido, incomum);		
 						print(message,"Parabéns! Você agora é <@&373640161290092544> \:trophy: \:ok_hand:");
 						
