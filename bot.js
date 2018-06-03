1a35ee6a4bab43af71c16d9fab42c01e38db922c
rslgp <rslgp@cin.ufpe.br>
2018-06-03 00:07:30 -0300

    msg uninstall completo

--- bot.js
+++ bot.js
@@ -1177,6 +1177,8 @@ client.on('message', message => {
 			//tirando bot do server
 			//message.guild.leave();
 			
+			print(message,"reifeltracker 100% desinstalado");
+			
 		break;
 		
 		case "help":
