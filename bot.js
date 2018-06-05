646fd93f8c61b4b3fbe3476986fe6e865fe39dd2
rslgp <rslgp@cin.ufpe.br>
2018-06-04 22:12:05 -0300

    visual msg tbquero

--- bot.js
+++ bot.js
@@ -47,7 +47,7 @@ const Browser = require('zombie');
 Browser.silent = true;
 Browser.waitDuration='6s'; //cloudflare
 //[apoia.se/reifel](https://apoia.se/reifel) - (*boleto | cartão de crédito - qlqr valor*)
-const rodape = "dono:Reifel#5047 !QueroEsseBot|!comandos", /*separador=" | ",*/ quebraLinha="\r\n", doacao=": tb use esse bot noutro discord com !tbQuero"+apoio;
+const rodape = "dono:Reifel#5047 !QueroEsseBot|!comandos", /*separador=" | ",*/ quebraLinha="\r\n", doacao=": use tb em outro discord - !tbQuero"+apoio;
 
 //tratando casos de erro
 const errorNickNaoEncontrado="nick não encontrado",
