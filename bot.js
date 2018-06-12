dc1ae8b5402514f89474a8ac7f4a4a12fafbfc51
rslgp <rslgp@cin.ufpe.br>
2018-06-12 13:44:37 -0300

    inserir usuariospremium

--- bot.js
+++ bot.js
@@ -10,9 +10,11 @@ const tabelaPreco = '**Mensalidade do bot ReifelTracker**\r\n*Depende da quantid
 
 const apoio = "";
 
+const usersPremium=['195731919424585728', '377626570816487449'];
+
 //"\r\n\r\ndá like pro fix da epic na escada q gira sozinha -> [clique aqui](https://accounts.epicgames.com/login/customized?regSubheading=Register&productCss=https%3A%2F%2Fwww.epicgames.com%2Ffortnite%2FssoAsset%2Ffortnite-custom.css&response_type=code&state=https%3A%2F%2Fwww.epicgames.com%2Ffortnite%2Fforums%2Fbugs-issues%2Fbug-reports%2F191591-stair-rotates-randomicaly-priorize-to-front-camera-and-only-rotate-if-pressed-r&client_id=52b63176173444eb8291b0dd60586e04&productName=fortnite&loginSubheading=Sign+In)";
 
-//setting up twitch
+//setting up twitch""
 const twitch = require("tmi.js");
 const twiconf = {
 	identity: {
@@ -763,7 +765,7 @@ client.on('message', message => {
 		break;
 
 		case "vs":
-		if(message.author!=reifelUser) return;
+		if(!( usersPremium.includes(message.author) )) return;
 		var jsonSquadPA,jsonSquadPB;
 		
 		var players = parametroUsado.split("%20x%20");
