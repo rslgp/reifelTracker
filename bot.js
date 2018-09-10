5206f72a6ca0ebeea409c60e4911288480209ae9
rslgp <rslgp@cin.ufpe.br>
2018-09-10 17:37:59 -0300

    remover msg convite gratis

--- bot.js
+++ bot.js
@@ -65,7 +65,7 @@ const winsStormShieldPath="body > div.container.pvp > div:nth-child(3) > div.col
 const comandoErrado = "comando inválido";
 //const AnunciarNovosPlanos="\r\n```md\r\n\r\n#22/05 agr +barato e para discord menores\r\n```";
 //const AnunciarNovosPlanos="\r\n```fix\r\n\r\n!semana grátis do bot\r\n```";
-const AnunciarNovosPlanos="\r\nUse grátis no seu servidor .convitegratis";
+const AnunciarNovosPlanos="";
 
 var refreshAuto = [];
 var refreshTamanho = 0;
