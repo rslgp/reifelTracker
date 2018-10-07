ed60b1cd71fc625dbb8f4c68806c3ac25c77eff6
rslgp <rslgp@cin.ufpe.br>
2018-10-07 09:49:21 -0300

    remover liberado free

--- bot.js
+++ bot.js
@@ -446,6 +446,7 @@ client.on('message', message => {
 	
 	if(idGuild!=363610360688672800){
 		if(!discAutorizados.includes(idGuild)){ //se cliente nao aplica
+			/*
 			if(message.content.includes("apoio")){
 				if(message.content.length == 6) {
 					message.author.send("Obrigado pelo interesse de apoiar, envie o comando apoio seguido de qualquer valor em reais (.apoio 5) ou envie msg privada para (@Reifel#5047 <@195731919424585728>),\r\ntem disponivel essas formas de pagamento: boleto, transferência bancária (banco do brasil), depósito, paypal");
@@ -479,8 +480,9 @@ client.on('message', message => {
 					break;
 				}		
 			}
+			*/
 			
-			//if(message.owner) message.owner.send("Não Autorizado por Reifel\r\n"+tabelaPreco); message.guild.leave(); console.log("sai"); return;
+			if(message.owner) message.owner.send("Não Autorizado por Reifel\r\n"+tabelaPreco); message.guild.leave(); console.log("sai"); return;
 		}
 	}
 	
