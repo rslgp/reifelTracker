4f6a8c8da2e868a6bfab219c956d821b170f5061
rslgp <rslgp@cin.ufpe.br>
2019-02-21 00:03:37 -0300

    26+

--- bot.js
+++ bot.js
@@ -3018,7 +3018,7 @@ function criarVoice(obj, i, max, message, name, permissoesOverwrites){
 	}, 100);		
 }
 
-function padraoRankWinApex(message, usuario, nickLegivel, winrKD, lendario, epico, raro, tabela=[78,48,20], continuaRank="Continua onde está, os niveis atuais são: 100, 78+, 48+, 20+"){
+function padraoRankWinApex(message, usuario, nickLegivel, winrKD, lendario, epico, raro, tabela=[78,48,26], continuaRank="Continua onde está, os niveis atuais são: 100, 78+, 48+, 26+"){
 		var maximo = "547963888256286732";
 		if(winrKD[0]==100){
 			if(usuario.roles.has(maximo)) {print(message,"Você está na patente máxima");return;}
