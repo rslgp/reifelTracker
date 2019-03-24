b1cc5694da161ebe5bc42ddc6da4959b3778c5e4
rslgp <rslgp@cin.ufpe.br>
2019-03-24 20:49:26 -0300

    msg pontuacao

--- bot.js
+++ bot.js
@@ -3659,7 +3659,7 @@ var att = (message.attachments).array();
 								//print(message,imgrResultado(parsedResult)); 
 								var lvlK = imgrResultado(parsedResult);
 								calcularPontuacao(message,lvlK);
-								print(message,"atualizei a pontuação");
+								print(message,"atualizei a pontuação"+lvlK[0]+" "+lvlK[1]);
 							}
 							).catch(err => {
 								console.error(err);
