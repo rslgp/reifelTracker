d3d106981983a48ade48dadd3d492761c539d95b
rslgp <rslgp@cin.ufpe.br>
2019-03-26 08:00:58 -0300

    fix msg

--- bot.js
+++ bot.js
@@ -225,7 +225,7 @@ client.on('ready', () => {
 	salaVotem = client.channels.get("413597195846156299");	
 	salaAposta = client.channels.get("416769967690743819");
 	reifelUser = client.users.get('195731919424585728');
-	client.user.setActivity("interessados: https://u.nu/reifelcontato |Reifel#5047 |.queroessebot", { type: 'LISTENING', url:'https://www.twitch.tv/reifel'});
+	client.user.setActivity("interessados: https://u.nu/reifelcontato |Reifel#5047 |.queroessebot", { type: 'WATCHING', url:'https://www.twitch.tv/reifel'});
 	/*client.user.setPresence({
 		game: {
 			//name: "com "+client.guilds.array().length +"|dono:Reifel#5047"
