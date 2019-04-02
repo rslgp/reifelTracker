2b5f6f0443760d8d1386117996233a7daac42b59
rslgp <rslgp@cin.ufpe.br>
2019-04-01 23:12:33 -0300

    fix throw crash

--- bot.js
+++ bot.js
@@ -3593,7 +3593,7 @@ function getNickConhecidoApexAMS(message){
 		var retorno = message.member.nickname.substring(0,  message.member.nickname.lastIndexOf(" ", posicaoGuardaChuva-2));
 		return retorno;
 	}else{
-		throw false;
+		return "";
 	}
 }
 
