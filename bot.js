455619ddefc3dad33a2385027ff16e9ad2c25699
rslgp <rslgp@cin.ufpe.br>
2019-04-08 18:35:04 -0300

    fix lvl sem nick

--- bot.js
+++ bot.js
@@ -1694,7 +1694,7 @@ client.on('message', message => {
 			}catch(e){
 				//caso nao tenha guarda chuva, mantem o nick como arg
 				if(args[1] == undefined) message.author.send("faltou colocar espaço o seu nick da origin no comando .lvl");
-				print(message,"ainda não registrado, envie o comando .lvl espaço seu nick da origin");
+				if((message.content).length > 4) print(message,"ainda não registrado, envie o comando .lvl espaço seu nick da origin");
 			}
 			site = "https://apex.tracker.gg/profile/pc/"+parametroUsado;
 			try{
