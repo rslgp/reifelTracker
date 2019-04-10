a34b2a13f1dad5dcde4ed3b9f4960cdabddae5e7
rslgp <rslgp@cin.ufpe.br>
2019-04-09 21:52:34 -0300

    fix msg elo top

--- bot.js
+++ bot.js
@@ -1433,9 +1433,9 @@ client.on('message', message => {
 			var resultadoJSON = top10ELO.toJSON();
 			var tamanho = top10ELO.size();
 			for(var i=0; i<tamanho; i++){
-				resultado += (i+1)+espaco+resultadoJSON[i+1].nick+espaco+resultadoJSON[i+1].elo+quebralinha;
+				resultado += (i+1)+espaco+resultadoJSON[i+1].nick+espaco+resultadoJSON[i+1].elo+quebraLinha;
 			}
-			print(message,"ELO Ranking:"+quebralinha+resultado);
+			print(message,"ELO Ranking:"+quebraLinha+resultado);
 		break;
 			
 		case "salvartabelaelo":
