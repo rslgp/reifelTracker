339a1319e3cc08c353798cefba9cf9df07633833
rslgp <rslgp@cin.ufpe.br>
2019-04-10 15:40:08 -0300

    fix atualizar nick tabela

--- bot.js
+++ bot.js
@@ -4313,7 +4313,7 @@ function LinkedList() {
 			head = node;
 		}else{
 			while(currentNode.next){
-				if(node.element.nick == currentNode.element.nick) {currentNode.element.elo = node.element.elo; break;}
+				if(node.element.nick == currentNode.element.nick) {currentNode.element.elo = node.element.elo; return;}
 				
 				if(node.element.elo > currentNode.next.element.elo){					
 					node.next = currentNode.next;
