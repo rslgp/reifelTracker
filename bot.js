1c701dd3e2bbd147aa84d381a8d12deccd6a9688
rslgp <rslgp@cin.ufpe.br>
2019-07-15 09:01:33 -0300

    fix bigwide

--- bot.js
+++ bot.js
@@ -636,7 +636,13 @@ client.on('message', message => {
 				if(w < 1360){
 					smallwide = 0.02;
 				}
-				cropReadImg(message, att[0].url, w, h, [ (0.12+wideFactor)*w , 0.08*h, (0.44+smallwide)*w, (0.55+aRFactor)*h], cargosimg, arrayIDcargosRead); //c = [cropLeft, cropTop, cropRight, cropBottom] in px			
+				
+				if(w>1900){ //big wide
+					cropReadImg(message, att[0].url, w, h, [ 0.05*w , 0.06*h, 0.33*w, 0.40*h], cargosimg, arrayIDcargosRead); //c = [cropLeft, cropTop, cropRight, cropBottom] in px			
+				}else{
+					cropReadImg(message, att[0].url, w, h, [ (0.12+wideFactor)*w , 0.08*h, (0.44+smallwide)*w, (0.55+aRFactor)*h], cargosimg, arrayIDcargosRead); //c = [cropLeft, cropTop, cropRight, cropBottom] in px			
+				}
+				
 			}catch(e){}
 		break;
 			
