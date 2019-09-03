cebd1421a2de33e419f670c07801d9538e8b2bb9
rslgp <rslgp@cin.ufpe.br>
2019-09-03 13:13:22 -0300

    release feature fila ranked bot

--- bot.js
+++ bot.js
@@ -240,9 +240,78 @@ client.on('guildCreate', guild => {
 	
 });
 
+
+//react
+//fila ranked bot
+var primeiroReagiu = false;
+var maxTempo, msgFila=null, qtdTickets;
+
+Date.prototype.addMinutes15 = function() {
+  this.setTime(this.getTime() + (900000));
+  return this;
+}
+
+function zerarFilaRankedBot(){ //qnd atingir jogadores necessarios ou acabar o tempo de 15 min da fila
+	primeiroReagiu=false; 
+	//apagar e depois reagir
+	msgFila.clearReactions().then( 
+		setTimeout(function(){msgFila.react(ticketRankedEmoji).catch(e=>null);},1000) 
+	);  
+}
+
+client.on('messageReactionAdd', (reaction, user) => {
+	//so funciona com mensagens criadas/editadas apos iniciar o client
+	/*
+	console.log(reaction);
+	console.log("\r\n####################\r\n");
+	console.log(user);
+	*/
+	if(user.bot) return;
+	switch(reaction.message.id){
+		case "617884991531122688": //msg de tickets do bot
+		
+			qtdTickets = reaction.message.reactions.get(ticketRankedEmoji);
+			if(qtdTickets!=null){
+				if(msgFila==null) msgFila = reaction.message;
+				
+				//se atingiu a qtd necessaria pra ranked
+				if(qtdTickets.count > 50){
+					var msgUsersMention="";
+					var u = qtdTickets.users;
+					zerarFilaRankedBot();
+					for(var i of u){
+					  if(i[1].bot === false) msgUsersMention+=i[1];					  
+					}
+					if(msgUsersMention!="") {
+						reaction.message.channel.send(msgUsersMention).then(mentionMsg => mentionMsg.delete(10000)).catch(e => null);
+					}
+				}
+			}
+			
+			//se foi o primeiro q reagiu criar a fila e preparar para resetar dps de 15 min
+			if(primeiroReagiu==false){
+				primeiroReagiu=true;
+				maxTempo = new Date();
+				maxTempo = maxTempo.addMinutes15();
+				//900 segundos eh 15 min
+				reaction.message.channel.send('Fila ranked iniciada, aguardando jogadores...\r\nesta fila será encerrada quando tiver 50 jogadores OU as: '+maxTempo.getHours()+":"+maxTempo.getMinutes())
+				  .then(msgContagem => 
+						setTimeout(function(){							
+							zerarFilaRankedBot();
+							msgContagem.delete();
+						}, 900000) )
+				  .catch(e => null);
+			}
+		break;
+	}
+	
+});
+//fim react
+
 client.on('ready', () => {
 	debug = client.channels.get('598226746701119711');
 	
+	/*
 	//reacao
 	client.channels.get("617882572743245863").fetchMessage('617884991531122688').then(message2 => {		
 		//message2.edit("reaja com :tickets: para entrar na fila ranked para ouro, platina, diamante ou predador\r\n:recycle: para ficar entrando automaticamente na fila\r\n:x: para parar de entrar automaticamente");
@@ -252,6 +321,7 @@ client.on('ready', () => {
 	} )
 	  .catch(e => null);
 	//fim-reacao
+	*/
 	
 	client.channels.get("459432939898273798").fetchMessage('616043152905863178')
 		  .then(message2 => {
