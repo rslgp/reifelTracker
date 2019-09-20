9ae27795a6206897e03abbf571d6d79908c2dd8d
rslgp <rslgp@cin.ufpe.br>
2019-09-20 01:07:25 -0300

    fix debug

--- bot.js
+++ bot.js
@@ -303,7 +303,7 @@ client.on('messageReactionAdd', (reaction, user) => {
 			
 		case "624424320919404544":
 			//se passaram 5 min da ultima checagem
-			debug.send(reaction.message, ((new Date().getTime()) - reaction.message.editedTimestamp));
+			debug.send(""+ ((new Date().getTime()) - reaction.message.editedTimestamp));
 			if( ((new Date().getTime()) - reaction.message.editedTimestamp) > 600){
 				reaction.message.clearReactions().then( 
 					setTimeout(function(){reaction.message.react("♻").catch(e=>null);},1000) 
