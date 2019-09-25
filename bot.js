cdb86901ffde4bbc41f563937a671b46f0f37785
rslgp <rslgp@cin.ufpe.br>
2019-09-25 08:30:16 -0300

    fix crash menu comandos

--- bot.js
+++ bot.js
@@ -37,7 +37,7 @@ var aprendizadoPausado=true;
 var top10ELO, topEloDesatualizado=[true,true];
 var debug;
 
-const menuComandos =["1 de 2 Apex Legends\n\n.lvl nick - registra e atualiza o lvl\n.elo - atualiza o tier\n.ranked - atualiza o rank\n.mudei nick - atualiza para novo nick\n.dk nick - checa kills e dano por foto\n.ondevou - mostra onde cair\n\n>> [Premium](https://catarse.me/reifeltracker)\n.lendas nick - lista por ordem de kill as lendas\n.ce nick - confere o tier", "2 de 2 Admin e Fortnite\n\nAdmin:\ncriar varias salas\nrenomear membro\ncargo Bloqueado\n\nFortnite:\n.t - para kd ou lvl\n.up - atualizar\n.solo\n.troquei"];
+const menuComandos =["1 de 2 Apex Legends - Comandos\n\n.lvl nick - registra e atualiza o lvl\n.elo - atualiza o tier\n.ranked - atualiza o rank\n.mudei nick - atualiza para novo nick\n.dk nick - checa kills e dano por foto\n.ondevou - mostra onde cair\n\n>> [Premium](https://catarse.me/reifeltracker)\n.lendas nick - lista por ordem de kill as lendas\n.ce nick - confere o tier", "2 de 2 Admin e Fortnite - Comandos\n\nAdmin:\ncriar varias salas\nrenomear membro\ncargo Bloqueado\n\nFortnite:\n.t - para kd ou lvl\n.up - atualizar\n.solo\n.troquei"];
 
 /*
 //setting up twitch
@@ -323,6 +323,7 @@ client.on('messageReactionAdd', (reaction, user) => {
 	if(reaction.message.author == client.user){
 		reaction.remove(user);
 		var conteudo = reaction.message.embeds[0].description, numero;
+		if(conteudo.contains("Comandos") == false) return;
 		var oldEmbed = reaction.message.embeds[0];
 		var newEmbed = {embed: 
 				{
