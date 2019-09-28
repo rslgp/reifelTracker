340449b5748ac9ac66284373b83f26d39611f58f
rslgp <rslgp@cin.ufpe.br>
2019-09-28 09:54:08 -0300

    circular help fix

--- bot.js
+++ bot.js
@@ -342,13 +342,15 @@ client.on('messageReactionAdd', (reaction, user) => {
 			case "➡":
 				numero = Number(conteudo.substring(0,1));
 				numero -= 1;
-				newEmbed.embed.description = menuComandos[(numero+1)%menuComandos.length];
+				numero = numero < menuComandos.length-1 ? i+1 : 0;
+				newEmbed.embed.description = menuComandos[numero];
 				reaction.message.edit(newEmbed);
 			break;
 			case "⬅":
 				numero = Number(conteudo.substring(0,1));
-				numero -= 1;
-				newEmbed.embed.description = menuComandos[(numero-1)%menuComandos.length];
+				numero -= 1;				
+				numero = i > 0 ? i-1 : menuComandos.length-1;
+				newEmbed.embed.description = menuComandos[numero];
 				reaction.message.edit(newEmbed);
 			break;
 			
