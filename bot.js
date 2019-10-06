e9f4ab374a4052a1ecb826a6e82f794395e46e3c
rslgp <rslgp@cin.ufpe.br>
2019-10-06 11:44:06 -0300

    msg novos preços

--- bot.js
+++ bot.js
@@ -4148,7 +4148,7 @@ function randomADS(){
 		if(diaVencimento > diaAtual){
 			msgBoletoAnonimo = "contribua no [projeto](https://www.catarse.me/reifeltracker) do bot pelo boleto em pdf: [R$ 5]("+boletoanonimo.boleto5.link+") | [R$ 12]("+boletoanonimo.boleto20.link+")  vencimento: "+boletoanonimo.boleto5.venc;
 		}else{
-			msgBoletoAnonimo = "contribua no [projeto](https://www.catarse.me/reifeltracker) do bot";			
+			msgBoletoAnonimo = "contribua no [projeto](https://www.catarse.me/reifeltracker) do bot (novos preços! todos baixaram dia 06/10)";			
 		}
 		ad.embed.description = msgBoletoAnonimo;
 		/*
