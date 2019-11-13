3883e9592549ec95e2389aa2e0df2ac0494d24eb
rslgp <rslgp@cin.ufpe.br>
2019-11-13 06:01:58 -0300

    msg apelativa doacao

--- bot.js
+++ bot.js
@@ -1100,8 +1100,8 @@ client.on('message', message => {
 		 //deixe passar
 		}else{		
 			if(!usersPremium.includes(message.author.id) && message.guild.id == '542501242916700181'){
-				var msgBoletoAnonimo = "boleto avulso (vencimento: "+boletoanonimo.boleto5.venc+") pdf: [R$ 5]("+boletoanonimo.boleto5.link+") \t|\t [R$ 12]("+boletoanonimo.boleto20.link+")\r\nPara mais opções de vencimentos acesse o [Boletos ReifelTracker](https://cdn.discordapp.com/attachments/625721376308723713/643494075894464543/Boletos_ReifelTracker.pdf)";
-				print(message, "Acabaram os créditos, contribua no catarse para TODOS continuarem usando\n[explicação: ]ao invés do dono do discord sozinho custear o funcionamento do bot,\nagora são as contribuições dos usuários que garantem, através do https://catarse.me/reifeltracker (boleto, cartão) com prêmios inclusos\r\n(R$5 viram "+(5*cred12/12).toFixed(0)+" créditos | R$12 são "+cred12+" = uma barra cheia) e após contribuir aguarde a conversão\nopcional: "+msgBoletoAnonimo);
+				var msgBoletoAnonimo = "boleto sem cadastrar (vencimento: "+boletoanonimo.boleto5.venc+") pdf: [R$ 5]("+boletoanonimo.boleto5.link+") \t|\t [R$ 12]("+boletoanonimo.boleto20.link+")\r\nPara mais pdf de boletos prontos acesse o [Boletos ReifelTracker](https://cdn.discordapp.com/attachments/625721376308723713/643494075894464543/Boletos_ReifelTracker.pdf)";
+				print(message, "Desculpe no momento esse comando não funciona,\r\npara voltar a funcionar eu preciso pagar os serviços e o dinheiro vem de doações feitas no: https://catarse.me/reifeltracker (boleto, cartão) os doadores serão premiados\r\na doação é convertida em tempo de funcionamento\numa forma fácil de doar é: "+msgBoletoAnonimo);
 				return;		
 			}
 		}
