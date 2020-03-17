89e5fbf8dd1bb7754c232c6134c507157f460ef4
rslgp <rslgp@cin.ufpe.br>
2020-03-17 10:16:39 -0300

    updateOtimOnline #2

--- bot.js
+++ bot.js
@@ -1125,7 +1125,7 @@ function executarComandos(message, comando, args, isDM, nickConhecido){
 	}
 	
 	//stats de uso
-	debug.send(message.createdTimestamp);
+	debug.send(new Date(message.createdTimestamp).getHours);
 	
 	var parametroUsado, nickLegivel, site;
 	
